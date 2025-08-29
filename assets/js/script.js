45595a875eec002a2afb1bc5884e78646c8cc89e
renan21-tg <rnantg21@gmail.com>
2025-08-29 20:39:17 -0300

    :sparkles: feat: Adicionando card de taks

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+function addTask () {
+    let taskName = document.getElementById('name').value
+    let taskCategory = document.getElementById('category').value
+    let taskPriority = document.getElementById('priority').value
+    let taskDate = document.getElementById('date').value
+
+}
