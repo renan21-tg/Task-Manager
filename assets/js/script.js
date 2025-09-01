88120684b5dbc9acb014c4302c6a23346df476ac
renan21-tg <rnantg21@gmail.com>
2025-09-01 10:34:38 -0300

    :sparkles: feat: Adicionando funcionalidade JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,4 +4,42 @@ function addTask () {
     let taskPriority = document.getElementById('priority').value
     let taskDate = document.getElementById('date').value
 
+    partData = taskDate.split('-')
+    taskDate = `${partData[2]}/${partData[1]}/${partData[0]}`
+
+    let list = document.getElementById('list')
+
+    let priorityColor = ''
+
+    switch (taskPriority) {
+        case 'Alta':
+            priorityColor = 'high'
+            break
+        case 'Média':
+            priorityColor = 'medium'
+            break
+        case 'Baixa':
+            priorityColor = 'low'
+            break
+    }
+
+    list.innerHTML += `<li>
+                            <div class="task-card">
+                                <div class="task-info">
+                                    <div class="task-title">${taskName}</div>
+                                    <div class="task-category">${taskCategory}</div>
+                                    <div class="task-date">Data Limite: <span>${taskDate}</span></div>
+                                    <div class="task-btns">
+                                        <button class="done-btn">Marcar como Concluida</button>
+                                        <button class="delete-btn">Deletar Tarefa</button>
+                                    </div>
+                                </div>
+                                <div class="task-priority-icon">
+                                    <div class="icon ${priorityColor}">${taskPriority}</div>
+                                </div>
+                            </div>
+                        </li> `
+    
+    document.querySelector('#name').value = ' '
 }
+
